1aefa906784791fbb3e7b8b5e222ec6647b2497b
madhuprakash19 <madhutp11@gmail.com>
2022-04-03 13:16:51 +0530

    heroku host commit

--- prank/settings.py
+++ prank/settings.py
@@ -24,9 +24,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'crush/templates')
 SECRET_KEY = 'django-insecure-2tfw7_ma3!h*d)c&nlh1b_u^61+6crfa14c2!)_056pgkcjw5n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
